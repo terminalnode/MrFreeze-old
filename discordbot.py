dd2338bf954d11fae3e537520dfeb9d935b47eb5
Terminal Node <monster@Buddha.local>
2018-07-14 21:39:29 +0200

    message -> ctx

--- discordbot.py
+++ discordbot.py
@@ -11,8 +11,8 @@ bot = commands.Bot(command_prefix='!')
 ### This will be used for functions which are planned
 ### for but not yet implemented to give users a heads up.
 ###
-async def not_implemented(message, command):
-    await message.channel.send(message.author.mention + " The command '" + command + "' isn't implemented yet. Try again some other time!")
+async def not_implemented(ctx, command):
+    await ctx.channel.send(ctx.author.mention + " The command '" + command + "' isn't implemented yet. Try again some other time!")
     return
 
 ###
@@ -40,56 +40,61 @@ async def on_ready():
 ### BANISH TO ANTARCTICA ###
 ############################
 @bot.command()
-async def banish(message, member: discord.Member):
-    if discord.utils.get(message.guild.roles, name='Administration') in message.author.roles:
-        await commandlog('SUCCESS\t Command "banish" issued by {0.author}, ID: '.format(message) + str(message.author.id))
-        await message.channel.send(member.mention + ' will be banished to the frozen hells of Antarctica for 5 minutes!')
-        await member.add_roles(discord.utils.get(message.guild.roles, name='Antarctica'))
+async def banish(ctx, member: discord.Member):
+    if discord.utils.get(ctx.guild.roles, name='Administration') in ctx.author.roles:
+        await commandlog('SUCCESS\t Command "banish" issued by {0.author}, ID: '.format(ctx) + str(ctx.author.id))
+        await ctx.channel.send(member.mention + ' will be banished to the frozen hells of Antarctica for 5 minutes!')
+        await member.add_roles(discord.utils.get(ctx.guild.roles, name='Antarctica'))
         await asyncio.sleep(5*60) # 5*60 seconds = 5 minutes
-        await member.remove_roles(discord.utils.get(message.guild.roles, name='Antarctica'))
+        await member.remove_roles(discord.utils.get(ctx.guild.roles, name='Antarctica'))
     else:
-        await commandlog('FAIL\t Command "banish" issued by {0.author}, ID: '.format(message) + str(message.author.id))
-        await message.channel.send('Sorry ' + message.author.mention + ', you need to be a mod to do that.'.format(message))
+        await commandlog('FAIL\t Command "banish" issued by {0.author}, ID: '.format(ctx) + str(ctx.author.id))
+        await ctx.channel.send('Sorry ' + ctx.author.mention + ', you need to be a mod to do that.'.format(ctx))
 
 #######################
 ######## ban ##########
 ### BAN FROM SERVER ###
 #######################
 @bot.command()
-async def ban(message, member: discord.Member):
-    await not_implemented(message, 'ban')
+async def ban(ctx, member: discord.Member):
+    await not_implemented(ctx, 'ban')
 
 ########################
 ######### kick #########
 ### KICK FROM SERVER ###
 ########################
 @bot.command()
-async def kick(message, member: discord.Member):
-    await not_implemented(message, 'kick')
+async def kick(ctx, member: discord.Member):
+    await not_implemented(ctx, 'kick')
 
 #################
 ##### mute ######
 ### MUTE USER ###
 #################
 @bot.command()
-async def mute(message, member: discord.Member):
-    await not_implemented(message, 'mute')
+async def mute(ctx, member: discord.Member):
+    await not_implemented(ctx, 'mute')
 
 #############################
 ########### rps #############
 ### ROCK, PAPER, SCISSORS ###
 #############################
 @bot.command()
-async def rps(message, choice):
-    await not_implemented(message, 'rps')
+async def rps(ctx, choice):
+    await not_implemented(ctx, 'rps')
 
 ##############################
 ######## temperature #########
 ### TEMPERATURE CONVERSION ###
 ##############################
 @bot.command()
-async def temperature(message, temp, unit):
-    await not_implemented(message, 'temperature')
+async def temperature(ctx, temp, unit):
+    await not_implemented(ctx, 'temperature')
+
+@bot.command()
+async def source(ctx):
+    await ctx.channel.send('''My source code is available at:
+https://github.com/kaminix/DrFreeze''')
 
 # Log setup in accordance with:
 # https://discordpy.readthedocs.io/en/rewrite/logging.html#logging-setup
